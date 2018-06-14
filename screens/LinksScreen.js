d8e92efce61f8e53feb38b619a1de0aae3531834
Ryan Magoon <mr.magoon5@gmail.com>
2018-06-14 00:10:33 -0400

    add pokedex red background to links screen

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import React from 'react'
+import { ScrollView, StyleSheet } from 'react-native'
+import { ExpoLinksView } from '@expo/samples'
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links',
-  };
+    title: 'Links'
+  }
 
   render() {
     return (
@@ -14,14 +14,13 @@ export default class LinksScreen extends React.Component {
            * content, we just wanted to provide you with some helpful links */}
         <ExpoLinksView />
       </ScrollView>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
-});
+    backgroundColor: '#dc0b2d'
+  }
+})
