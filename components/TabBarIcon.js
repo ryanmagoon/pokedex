60c74511edba0501500cbe475349bdf07b44c4ab
Ryan Magoon <rmagoon@6365.local>
2018-06-20 23:00:35 -0400

    support pokeball in tabicon

--- components/TabBarIcon.js
+++ components/TabBarIcon.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import { Ionicons } from '@expo/vector-icons';
-import Colors from '../constants/Colors';
+import React from 'react'
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons'
+import Colors from '../constants/Colors'
 
 export default class TabBarIcon extends React.Component {
   render() {
-    return (
+    return this.props.name === 'pokeball' ? (
+      <MaterialCommunityIcons
+        name={this.props.name}
+        size={26}
+        style={{ marginBottom: -3 }}
+        color={
+          this.props.focused ? Colors.tabIconSelected : Colors.tabIconDefault
+        }
+      />
+    ) : (
       <Ionicons
         name={this.props.name}
         size={26}
         style={{ marginBottom: -3 }}
-        color={this.props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+        color={
+          this.props.focused ? Colors.tabIconSelected : Colors.tabIconDefault
+        }
       />
-    );
+    )
   }
 }
