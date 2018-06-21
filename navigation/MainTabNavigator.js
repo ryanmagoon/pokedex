a9b0c6cb7286e63d702f533e948ceddad24ab095
Ryan Magoon <rmagoon@6365.local>
2018-06-21 00:12:11 -0400

    fix package ref

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,4 +1,4 @@
-import { createStackNavigator } from '../../../Library/Caches/typescript/2.9/node_modules/@types/react-navigation'
+import { createStackNavigator } from 'react-navigation'
 
 import HomeScreen from '../screens/HomeScreen'
 import ListScreen from '../screens/ListScreen'
