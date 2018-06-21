6ce724cac9cb5ae46c523d81d447c673dee9742e
Ryan Magoon <rmagoon@6365.local>
2018-06-21 00:11:28 -0400

    rename links screen to list screen

--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -1,8 +1,8 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import { Dimensions, Image, ScrollView, StyleSheet } from 'react-native'
 import { Query } from 'react-apollo'
 import { gql } from 'apollo-boost'
-import { Card, CardItem, Text, Body } from 'native-base'
+import { Card, CardItem, Text, Body, View } from 'native-base'
 
 const screenWidth = Dimensions.get('window').width
 
@@ -21,21 +21,43 @@ const cardStyle = { margin: 15, height: 320, width: 280 }
 
 export default class LinksScreen extends Component {
   static navigationOptions = {
-    title: 'Links'
+    title: 'Links',
+    header: null
   }
 
   render = () => (
     <Query query={getPokemons}>
       {({ loading, error, data: { pokemons } }) => {
-        if (loading) return <Text>Loading...</Text>
+        if (loading)
+          return (
+            <View
+              style={{
+                backgroundColor: '#dc0b2d',
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center'
+              }}
+            >
+              <Text style={styles.text}>Loading...</Text>
+            </View>
+          )
         if (error) return `Error! ${error.message}`
 
         return (
-          <ScrollView style={{ flex: 1 }}>
+          <ScrollView
+            contentContainerStyle={{
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}
+            style={{
+              backgroundColor: '#dc0b2d',
+              flex: 1
+            }}
+          >
             {pokemons.map(({ name, number, image }, i) => (
-              <Card key={i}>
+              <Card key={i} style={{ width: '90%' }}>
                 <CardItem header>
-                  <Text>{number}</Text>
+                  <Text style={styles.text}>{number}</Text>
                 </CardItem>
                 <CardItem>
                   <Body>
@@ -44,13 +66,13 @@ export default class LinksScreen extends Component {
                       style={{
                         resizeMode: 'contain',
                         height: 0.55 * screenWidth,
-                        width: 0.95 * screenWidth
+                        width: '95%'
                       }}
                     />
                   </Body>
                 </CardItem>
                 <CardItem footer>
-                  <Text>{name}</Text>
+                  <Text style={styles.text}>{name}</Text>
                 </CardItem>
               </Card>
             ))}
@@ -65,5 +87,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#dc0b2d'
+  },
+  text: {
+    fontFamily: 'pokemon'
   }
 })
